0d1ebab6999b138b88c41dd2c9e38071ab59dc90
eneriopaolo <paoloenerio@gmail.com>
2024-02-12 14:01:04 +0800

    Updated Log Out Function

--- TwitterCloneUi/eHit/homePage_api.js
+++ TwitterCloneUi/eHit/homePage_api.js
@@ -13,6 +13,8 @@ function getPost(){
     
 }
 
-function debug(){
-
+function logOut(){
+  document.cookie = "loginToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/";
+  localStorage.clear();
+  window.location.href = "../Login-Signup/login.html";
 }
